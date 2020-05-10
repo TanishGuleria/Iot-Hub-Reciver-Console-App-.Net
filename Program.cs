aa6cd2310ac1cd69e4c52a8c301b4e1c66785c51
tanish guleria <tanishguleria47@gmail.com>
2020-05-11 00:15:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Amqp;
+using Microsoft.Azure.Devices;
+namespace IotHubReciver
+{
+    class Program
+    {
+        static ServiceClient serviceClient;
+        static string connectionString = "HostName=tsgiotdevice.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=uaxE25fpJzDBk+Vs2GhAZUfJOKccC3njedzC2NTLXU0=";
+        static void Main(string[] args)
+        {
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            ReceiveFeedbackAsync();
+            Console.ReadLine();
+        }
+        private async static void ReceiveFeedbackAsync()
+        {
+            var feedbackReceiver = serviceClient.GetFeedbackReceiver();
+
+            Console.WriteLine("\nReceiving c2d feedback from service");
+            while (true)
+            {
+                var feedbackBatch = await feedbackReceiver.ReceiveAsync();
+                if (feedbackBatch == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received feedback: {0}",
+                  string.Join(", ", feedbackBatch.Records.Select(f => f.StatusCode)));
+                Console.ResetColor();
+
+                await feedbackReceiver.CompleteAsync(feedbackBatch);
+            }
+        }
+    }
+}
